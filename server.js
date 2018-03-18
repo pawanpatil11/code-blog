6c27082aff5d60e8d9b7651f9c725d1667e4ac49
pawan <iitsunbeam@gmail.com>
2018-03-18 11:52:40 +0530

    Second commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+console.log('server started listening on port 80!!!');
+app.listen(80);
